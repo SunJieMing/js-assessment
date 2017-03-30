7dad74243cf632dc5ff131478ff49e032e9bb344
Ben Nelson <benn@ifit.com>
2017-03-30 14:08:44 -0600

    Added questions and readme info

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# LambdaSchool Assessment
+
+## Instructions
+---
+Follow the instructions in `script.js`.  The problems become progressively more difficulut and you are not expected to be able to answer all of them.  Don't worry if there are concepts you don't understand or if you are unable to finish the assessment.  Our purpose is to get a basic idea of roughly how much preparation you would need before starting one of our classes.
+
+
+For more information about our other courses visit: https://www.lambdaschool.com
---
+++ script.js
@@ -0,0 +1,65 @@
+//#1
+//Declare a variale called 'name' that has the value of your name.
+
+/*code here*/
+
+//#2
+//create an if statement that checks to see if your name is equal to 'Ben'
+//If yes, log 'yes'
+//otherwise log 'no'
+//note: use console.log to log the string
+
+/*code here*/
+
+//#3
+//create an array called 'favoriteFoods'.
+//fill it up with the names of several of your favorite foods
+
+/*code here*/
+
+//#4
+//use a for loop to log each food in the 'favoriteFoods' array
+
+/*code here*/
+
+//#5
+//create an object called 'favoriteMovie'.
+//give 'favoriteMovie' a property called 'runtime' and set it equal to how long the movie is in minutes
+//give 'favoriteMovie' a property called 'title' and set it equal to the title
+//give 'favoriteMovie' a property called 'director' and set it equal to the director's name
+
+/*code here*/
+
+//#6
+//create a function called 'sayHi'.
+//'sayHi' should accept one argument called 'name'
+//when 'sayHi' is invoked it should log the string 'Hello <name>!' where <name> is equal to the 'name' argument
+
+/*code here*/
+
+//#7
+//create an array called 'friends'
+//create three objects that have information about your friends
+//each object should have a 'name', 'age', and 'vocation' property with appropriate data
+//put the three objects inside of the 'friends' array
+
+/*code here*/
+
+//#8
+//use a for loop to iterate over the 'friends' array from problem #7
+//inside the for loop print the string 'My friend <name> is <age> years old and does <vocation> for work.'
+
+/*code here*/
+
+//#9
+//create a constructor called 'User' that can function as a class for creating new user objects
+//'User' should take 'email', and 'password' as arguments 
+//each instance of 'User' that is created should have a 'email', and 'password' property that is equal to the arguments passed to the constructor
+
+/*code here*/
+
+//#10
+//create a function called 'nFactorial(num)'
+//'nFactorial' should return the factorial of the 'num' argument
+//note: the factorial of 5 is (5 * 4 * 3 * 2 * 1) ->  120
+//try to use recursion
